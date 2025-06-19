2d1ca3a3d7a11907ff4b0750b9f576af22acd39f
Lee Dong Chan <ehdcks032@naver.com>
2025-06-19 10:53:48 +0900

    Jsp02_Board 기능 추가
    리스트 전제 조회 페이지 생성 및 전체 조회 기능 추가

--- 5_Web/Jsp02_Board/WebContent/boardlist.jsp
+++ 5_Web/Jsp02_Board/WebContent/boardlist.jsp
@@ -1,5 +1,13 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+    
+<% request.setCharacterEncoding("UTF-8"); %>
+<% response.setContentType("text/html; charset=UTF-8"); %>
+
+<%@ page import="com.board.dao.BoardDao" %>
+<%@ page import="com.board.dto.BoardDto" %>
+<%@ page import="java.util.List" %>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +15,46 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+	<!-- db에 저장된 데이터를 모두 조회하여
+		 화면에 목록으로 보여준다. -->
+		 
+<%
+	BoardDao dao = new BoardDao();
+	List<BoardDto> list = dao.selectAll();
+%>
+<h1>List</h1>
+<table>
+	<col width="30px"><col width="50px"><col width="100px"><col width="300px"><col width="100px">
+	<tr>
+		<th><input type="checkbox"></th>
+		<th>SEQ</th>
+		<th>WRITER</th>
+		<th>TITLE</th>
+		<th>DATE</th>
+	</tr>
+<%
+	if(list.size() == 0){
+		//조회한 게시글이 0개일 때 (없다).
+%>
+	<tr>
+		<td colspan="5">-----글이 존재하지 않습니다-----</td>
+	</tr>
+<%
+	}else{
+		// 조회한 데이터를 화면에 출력
+		for(BoardDto dto : list){	// 향상된 for 문
+%>
+			<tr>
+				<td><input type="checkbox"></td>
+				<td><%=dto.getSeq() %></td>
+				<td><%=dto.getWriter() %></td>
+				<td><%=dto.getTitle() %></td>
+				<td><%=dto.getRegdate() %></td>
+			</tr>
+<%
+		}
+	}
+%>
+</table>
 </body>
 </html>
--- 5_Web/Jsp02_Board/src/com/board/dao/BoardDao.java
+++ 5_Web/Jsp02_Board/src/com/board/dao/BoardDao.java
@@ -1,5 +1,12 @@
 package com.board.dao;
 
+import static common.JDBCTemplate.*;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.board.dto.BoardDto;
@@ -7,7 +14,42 @@ import com.board.dto.BoardDto;
 public class BoardDao {
 	// 전체 조회
 	public List<BoardDto> selectAll(){
-		return null;
+		Connection con = getConnection();
+		Statement stmt = null;	// 쿼리문을 실행하는 객체
+		ResultSet rs = null;	// 결과값을 담을 객체  (ROW 하나)
+		List<BoardDto> res = new ArrayList<>(); 	// 위에서 나온 ROW 하나하나의 결과값들을 모으기 위한 리스트
+		
+		String sql = " SELECT * FROM BOARD "
+					+" ORDER BY SEQ DESC ";
+		
+		try {
+			stmt = con.createStatement();
+			System.out.println("03.query 준비" + sql); 		// 단순 진행과정 확인용
+			
+			rs = stmt.executeQuery(sql);
+			System.out.println("04. query 실행 및 리턴");
+			
+			while(rs.next()) {
+				// 결과 값을 rs에 담고, 그 한줄 한줄을 리스트에 추가
+				BoardDto tmp = new BoardDto();		// 결과 값을 Dto 객체에 담고,
+				tmp.setSeq(rs.getInt(1));
+				tmp.setWriter(rs.getString(2));
+				tmp.setTitle(rs.getString("TITLE"));
+				tmp.setContent(rs.getString(4));
+				tmp.setRegdate(rs.getDate(5));
+				
+				res.add(tmp);			// 리스트에 저장.
+			}
+		} catch (SQLException e) {
+			System.out.println("3/4 단계 에러");
+			e.printStackTrace();
+		}finally {
+			close(rs);
+			close(stmt);
+			close(con);
+			System.out.println("05.db 종료");
+		}
+		return res;
 	}
 	
 	// 선택 조회
