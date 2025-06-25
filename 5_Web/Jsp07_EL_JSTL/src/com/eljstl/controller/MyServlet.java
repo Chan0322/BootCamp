33076c379f94f4785ec674ddbe3fb4e427e79f4a
Lee Dong Chan <ehdcks032@naver.com>
2025-06-25 13:21:46 +0900

    5_Web - Jsp07_EL_JSTL
    Servlet 연습
    EL(Expression Language)

---
+++ 5_Web/Jsp07_EL_JSTL/src/com/eljstl/controller/MyServlet.java
@@ -0,0 +1,30 @@
+package com.eljstl.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.eljstl.score.Score;
+
+@WebServlet("/myservlet.do")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Score sc = new Score("이창진",100,77,56);
+		
+		request.setAttribute("dto", sc);
+		RequestDispatcher disp = request.getRequestDispatcher("eltest.jsp");
+		disp.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
