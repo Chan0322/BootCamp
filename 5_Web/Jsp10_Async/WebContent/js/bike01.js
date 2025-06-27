1da4db8f02bd16a732a7bd0f619c72ab55c8d0e4
Lee Dong Chan <ehdcks032@naver.com>
2025-06-27 16:38:43 +0900

    5_Web - Jsp10_Async
    JSON 데이터에 접근해서 화면에 띄우기

---
+++ 5_Web/Jsp10_Async/WebContent/js/bike01.js
@@ -0,0 +1,50 @@
+$(function(){
+	getJson();
+});
+
+function getJson(){
+	//alert("getJson()!! ");
+	
+	//getJSON통해 json 데이터 받아온다.
+	$.getJSON("json/bike.json", function(data){
+		//console.log(data);
+		$.each(data, function(key, val){
+			//console.log(key);
+			//console.log(val);
+			//console.log("0--------------")
+			
+			if(key=="DESCRIPTION"){
+				$("table").attr("border", "1");
+				
+				$("thead").append(
+					"<tr>"+
+						"<th>"+val.ADDR_GU+"</th>"+
+						"<th>"+val.CONTENT_ID+"</th>"+
+						"<th>"+val.CONTENT_NM+"</th>"+
+						"<th>"+val.NEW_ADDR+"</th>"+
+						"<th>"+val.CRADLE_COUNT+"</th>"+
+						"<th>"+val.LONGITUDE+"</th>"+
+						"<th>"+val.LATITUDE+"</th>"+
+					"</tr>"
+				);
+			}else {
+				// key가 == 'DATA'일 때 실행
+				for(let i=0; i<val.length; i++){
+					//str : 배열 안에 있는 하나의 json 데이터
+					let str = val[i];
+					$("tbody").append(
+						"<tr>"+
+							"<td>"+str.addr_gu+"</td>"+
+							"<td>"+str.content_id+"</td>"+
+							"<td>"+str.content_nm+"</td>"+
+							"<td>"+str.new_addr+"</td>"+
+							"<td>"+str.cradle_count+"</td>"+
+							"<td>"+str.longitude+"</td>"+
+							"<td>"+str.latitude+"</td>"+
+						"</tr>"
+					);
+				}
+			}
+		});
+	});
+}
---
+++ 5_Web/Jsp10_Async/src/com/test/controller/BikeServlet.java
@@ -0,0 +1,33 @@
+package com.test.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/bike.do")
+public class BikeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		String command = request.getParameter("command");
+		System.out.println("command: "+command);
+		
+		if(command.equals("first")) {
+			response.sendRedirect("bike01.jsp");
+			
+		}else if(command.equals("second")) {
+			response.sendRedirect("bike02.jsp");
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
