38db07d8e7ec7acfc648ee4c997976d265825623
Lee Dong Chan <ehdcks032@naver.com>
2025-06-30 13:06:11 +0900

    5_Web - Jsp10_Async
    클라이언트 측에서 (화면) 비동기통신(ajax)으로 서버와 통신해
    DB에 데이터 insert, 화면에 출력

--- 5_Web/Jsp10_Async/src/com/test/controller/BikeServlet.java
+++ 5_Web/Jsp10_Async/src/com/test/controller/BikeServlet.java
@@ -1,6 +1,10 @@
 package com.test.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,6 +14,8 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.test.dao.BikeDao;
+import com.test.dto.BikeDto;
 
 @WebServlet("/bike.do")
 public class BikeServlet extends HttpServlet {
@@ -29,6 +35,8 @@ public class BikeServlet extends HttpServlet {
 			response.sendRedirect("bike02.jsp");
 			
 		}else if(command.equals("second_db")) {
+			new BikeDao().deleteAll();
+			
 			String obj = request.getParameter("obj");
 			//System.out.println(obj);
 			
@@ -36,16 +44,76 @@ public class BikeServlet extends HttpServlet {
 			JsonElement element = parser.parse(obj);
 			
 			//DATA의 첫 번째 방에 저장된 json 데이터 new_addr 값 저장하기.
-			String str = element.getAsJsonObject().get("DATA").getAsJsonArray().get(0).getAsJsonObject().get("new_addr").getAsString();
-			System.out.println(str);
+//			String str = element.getAsJsonObject().get("DATA").getAsJsonArray().get(0).getAsJsonObject().get("new_addr").getAsString();
+//			System.out.println(str);
+//			
+//			// DESCRIPTION이 가지는 data의 ADDR_GU 의 값을 String 으로 저장하기.
+//			str = element.getAsJsonObject().get("DESCRIPTION").getAsJsonObject().get("ADDR_GU").getAsString();
+//			System.out.println(str);
 			
-			// DESCRIPTION이 가지는 data의 ADDR_GU 의 값을 String 으로 저장하기.
-			str = element.getAsJsonObject().get("DESCRIPTION").getAsJsonObject().get("ADDR_GU").getAsString();
-			System.out.println(str);
+			List<BikeDto> list = new ArrayList<>();
 			
-			for(int i=0; i<element.getAsJsonObject().get("DATA").getAsJsonArray().size(); i+=300) {
+			for(int i=0; i<element.getAsJsonObject().get("DATA").getAsJsonArray().size(); i++) {
 				JsonObject tmp = element.getAsJsonObject().get("DATA").getAsJsonArray().get(i).getAsJsonObject();
-				System.out.println(tmp.get("new_addr").getAsString());
+//				System.out.println(tmp.get("new_addr").getAsString());
+				String addr_gu = tmp.get("addr_gu").getAsString();
+				int content_id = tmp.get("content_id").getAsInt();
+				String content_nm = tmp.get("content_nm").getAsString();
+				String new_addr = tmp.get("new_addr").getAsString();
+				int cradle_count = tmp.get("cradle_count").getAsInt();
+				double longitude = tmp.get("longitude").getAsDouble();
+				double latitude = tmp.get("latitude").getAsDouble();
+				
+				BikeDto dto = new BikeDto();
+				dto.setAddr_gu(addr_gu);
+				dto.setContent_id(content_id);
+				dto.setContent_nm(content_nm);
+				dto.setNew_addr(new_addr);
+				dto.setCradle_count(cradle_count);
+				dto.setLongitude(longitude);
+				dto.setLatitude(latitude);
+				
+				list.add(dto);
+			}
+			
+			int res = new BikeDao().insert(list);
+			if(res == list.size()) {
+				System.out.println("insert 성공\n");
+			}else {
+				System.out.println("insert 실패\n");
+			}
+			PrintWriter out = response.getWriter();
+			out.println(res);
+			
+			
+		}else if(command.equals("first_db")) {
+//			System.out.println(request.getParameter("bike"));	// 동일한 이름의 파라미터 값은 1개만 받을 수 있음.
+			String[] bikeList = request.getParameterValues("bike");
+			System.out.println("bikelist의 크기: " + bikeList.length);	// 파라미터 값으로 받을 수 있는 최대 갯수는 999
+			
+			List<BikeDto> list = new ArrayList<>();
+			for(int i=0; i<bikeList.length; i++) {
+				//강남구/2301/ 현대고등학교 건너편/서울특별시 강남구 압구정로 134/10/127.02179/37.524071
+				String[] tmp = bikeList[i].split("/");
+				BikeDto dto = new BikeDto();
+				dto.setAddr_gu(tmp[0]);
+				dto.setContent_id(Integer.parseInt(tmp[1]));
+				dto.setContent_nm(tmp[2]);
+				dto.setNew_addr(tmp[3]);
+				dto.setCradle_count(Integer.parseInt(tmp[4]));
+				dto.setLongitude(Double.parseDouble(tmp[5]));
+				dto.setLatitude(Double.parseDouble(tmp[6]));
+				
+				list.add(dto);
+			}
+			new BikeDao().deleteAll();
+			int res = new BikeDao().insert(list);
+			if(res>0) {
+				System.out.println("insert 성공\n");
+				response.sendRedirect("index.html");
+			}else {
+				System.out.println("insert 실패\n");
+				response.sendRedirect("bike01.jsp");
 			}
 		}
 	}
