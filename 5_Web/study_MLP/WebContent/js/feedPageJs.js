c01e3c752dda09b1409afc78eb453ff0544d08fb
Lee Dong Chan <ehdcks032@naver.com>
2025-07-03 09:01:44 +0900

    study_MLP
    로그인 페이지 제작

---
+++ 5_Web/study_MLP/WebContent/js/feedPageJs.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', function(){   // html 로드 후 실행
+    const upBtn = document.getElementById('goTopBtn');
+
+    upBtn.addEventListener('click', function(){
+        // 바디 부분 스크롤
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+
+        // 내부 좌측 스크롤
+        const leftScroll = document.getElementById('leftItem');
+        leftScroll.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+
+    const newFeedBtn = document.querySelector('.createFeed');
+    const feedList = document.querySelector('.feedList');
+
+    newFeedBtn.addEventListener('click', function(){
+        const newFeedHtml = `
+            <div class="feedContent">
+                <img src="../src/feedSample1.png" alt="New Feed">
+            </div>
+        `;
+
+        // 피드 추가 -> 기존 목록 끝에 새 피드를 넣는다.
+        feedList.insertAdjacentHTML('beforeend', newFeedHtml);
+    });
+
+    // 프로필 수정 팝업
+    const editBtn = document.querySelector('.editProfile');
+    const popup = document.getElementById('editProfilePopupId');
+    const cancelBtn = document.getElementById('cancelEditBtn');
+    const saveBtn = document.getElementById('saveEditBtn');
+
+    // 팝업 열기
+    editBtn.addEventListener('click', function(){
+        popup.classList.add('show');
+    });
+    // 닫기
+    cancelBtn.addEventListener('click', function () {
+        popup.classList.remove('show');
+    });
+    // 저장 버튼
+    saveBtn.addEventListener('click', function () {
+        alert('프로필이 저장되었습니다!');
+        popup.classList.remove('show');
+    });
+
+});
