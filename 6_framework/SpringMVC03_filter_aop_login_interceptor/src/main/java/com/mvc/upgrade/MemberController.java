00de97ed2c00ad11e8f64d4b58fdde9cb48d7ddd
Lee Dong Chan <ehdcks032@naver.com>
2025-07-11 17:05:24 +0900

    6_framework - SpringMVC03_filter_aop_login_interceptor
    Interceptor 활용해보기.
    Interceptor를 통해 로그인 요청 체크 후 처리

---
+++ 6_framework/SpringMVC03_filter_aop_login_interceptor/src/main/java/com/mvc/upgrade/MemberController.java
@@ -0,0 +1,53 @@
+package com.mvc.upgrade;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.mvc.upgrade.model.dto.MemberDto;
+import com.mvc.upgrade.model.service.MemberService;
+
+@Controller
+public class MemberController {
+
+	private Logger logger = LoggerFactory.getLogger(MemberController.class);
+	
+	@Autowired
+	private MemberService service;
+	
+	@RequestMapping("/loginform.do")
+	public String loginForm() {
+		logger.info("LOGIN FORM");
+		return "mvclogin";
+	}
+	
+	//ResponseBody ; 페이지를 응답하는게 아닌 데이터를 응답한다!
+	@RequestMapping("/ajaxlogin.do")
+	@ResponseBody
+	public Map<String, Boolean> login(@RequestBody MemberDto dto, HttpSession session) {
+		logger.info("LOGIN");
+//		System.out.println(dto.getMemberid());
+//		System.out.println(dto.getMemberpw());
+		MemberDto res = service.login(dto);
+		
+		boolean check = false;
+		if(res != null) {
+			check=true;
+			session.setAttribute("login", res);
+		}
+		Map<String, Boolean> map = new HashMap<String, Boolean>();
+		map.put("check", check);
+		System.out.println("check: " + check);
+		
+		return map;
+	}
+}
