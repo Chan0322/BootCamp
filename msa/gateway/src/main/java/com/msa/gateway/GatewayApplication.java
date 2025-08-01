9385139248d2611eda784101865fa614ebfba1d2
Lee Dong Chan <ehdcks032@naver.com>
2025-08-01 13:47:43 +0900

    msa 실습
    Config Server
    
    중요한 정보들을 센터로 모아놓고 거기서 관리하는 기술.
    야물 파일이나 프로퍼티즈 파일을 따로 관리.....
    Git(형상관리 툴 ex) Git private, github, gitlab)을 통해 관리. (회사는 공개된 github에는 안올림)
    이 형상관리 툴을 통해 올려진 파일들을 Config Server를 통해 연결해 관리.

--- msa/gateway/src/main/java/com/msa/gateway/GatewayApplication.java
+++ msa/gateway/src/main/java/com/msa/gateway/GatewayApplication.java
@@ -1,20 +1,28 @@
 package com.msa.gateway;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@RefreshScope
 public class GatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
-	}
 	// 스프링부트의 기본 포트는 8080
+
+	@Value("${rest.api.url1}")
+	private String url1;
+
+	@Value("${rest.api.url2}")
+	private String url2;
+
 	@Bean
 	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		System.out.println(url1+":"+url2);
 		return builder.routes()
 				.route(p -> p
 						.path("/posts/**")	// posts로 시작하는 모든 path
@@ -23,5 +31,9 @@ public class GatewayApplication {
 						.path("/comments/**")	// comments로 시작하는 모든 path
 						.uri("http://localhost:8082"))
 				.build();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
 	}
 }
