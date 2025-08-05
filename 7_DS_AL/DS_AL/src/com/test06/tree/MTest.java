3a95c54e72dc3df75abd25154088baf1e9962e2c
Lee Dong Chan <ehdcks032@naver.com>
2025-08-05 17:49:01 +0900

    알고리즘
    이진 검색 트리

---
+++ 7_DS_AL/DS_AL/src/com/test06/tree/MTest.java
@@ -0,0 +1,41 @@
+package com.test06.tree;
+
+public class MTest {
+
+	public static void main(String[] args) {
+		BinarySearchTree<Integer, Data> tree = new BinarySearchTree<>();
+		
+		tree.add(5, new Data(5,"lcj"));
+		tree.add(10, new Data(10,"jsd"));
+		tree.add(1, new Data(1,"ksj"));
+		tree.add(12, new Data(12,"kkk"));
+		tree.add(8, new Data(8,"hhh"));
+		tree.add(13, new Data(13,"hhh"));
+		tree.add(9, new Data(9,"hhh"));
+		tree.print();
+	
+		System.out.println("----------------");
+		tree.remove(10);
+		tree.print();
+		
+		System.out.println("----------------");
+		
+		System.out.println("search: " + tree.search(12));
+	}
+
+}
+
+class Data{
+	private Integer no; 
+	public String name;
+	public Data(Integer no, String name) {
+		this.no = no;
+		this.name = name;
+	}
+	Integer keyCode() {
+		return no;
+	}
+	public String toString() {
+		return name;
+	}
+}
