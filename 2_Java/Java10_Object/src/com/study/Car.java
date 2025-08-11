07f605b354c16d6bbaef73b5f63d64119bb00d36
Lee Dong Chan <ehdcks032@naver.com>
2025-08-12 08:56:42 +0900

    생성자 선언 및 활용 연습

---
+++ 2_Java/Java10_Object/src/com/study/Car.java
@@ -0,0 +1,15 @@
+package com.study;
+
+public class Car {
+	String color;
+	String transmission;
+	String engine;
+	
+	// 생성자 선언
+	public Car(String color, String transmission, String engine) {
+		this.color = color;
+		this.transmission = transmission;
+		this.engine = engine;
+	}
+
+}
