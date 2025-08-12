002a39c0dd33e4302b8a95adb0b9336df579e153
Lee Dong Chan <ehdcks032@naver.com>
2025-08-12 13:04:35 +0900

    리액트
    state

--- 8_React/ReactTest01/src/App2.jsx
+++ 8_React/ReactTest01/src/App2.jsx
@@ -1,9 +1,39 @@
+//1. import
 import { useState } from "react";
 
+const Body = ({ st, test }) => {
+  return (
+    <div>
+      <h1>
+        Body입니다. state값: {st}, test값: {test}
+      </h1>
+    </div>
+  );
+};
+
 function App2() {
+  //2. state 생성
+  const [state, setState] = useState(0);
+
+  const [test, setTest] = useState("TEST");
+
+  let cnt = 0;
   return (
     <>
-      <h1>App2</h1>
+      <h1>state:{state}</h1>
+      <h1>var:{cnt}</h1>
+      <Body st={state} test={test} />
+      <hr></hr>
+
+      <button
+        onClick={() => {
+          setState(state + 1);
+          cnt = cnt + 1;
+          console.log("cnt: " + cnt);
+        }}
+      >
+        클릭
+      </button>
     </>
   );
 }
