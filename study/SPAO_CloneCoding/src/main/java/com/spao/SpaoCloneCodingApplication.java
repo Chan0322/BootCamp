d595af05935ac50c59f34771f2a2a4d87f97c250
Lee Dong Chan <ehdcks032@naver.com>
2025-08-14 00:30:57 +0900

    클론코딩_스파오
    프로젝트 생성 및 기본 세팅

---
+++ study/SPAO_CloneCoding/src/main/java/com/spao/SpaoCloneCodingApplication.java
@@ -0,0 +1,13 @@
+package com.spao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaoCloneCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaoCloneCodingApplication.class, args);
+	}
+
+}
---
+++ study/SPAO_CloneCoding/src/test/java/com/spao/SpaoCloneCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaoCloneCodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
