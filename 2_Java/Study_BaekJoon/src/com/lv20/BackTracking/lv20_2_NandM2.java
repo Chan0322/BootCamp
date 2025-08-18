3353cc82e075986d81fc9e32ee2fffb1aaf62c59
Lee Dong Chan <ehdcks032@naver.com>
2025-08-18 09:03:17 +0900

    study 알고리즘

---
+++ 2_Java/Study_BaekJoon/src/com/lv20/BackTracking/lv20_2_NandM2.java
@@ -0,0 +1,47 @@
+package com.lv20.BackTracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class lv20_2_NandM2 {
+
+	public static void main(String[] args) throws IOException {
+		nAndM2();
+	}
+
+	static int n, m;
+	static int[] res;
+	
+	public static void nAndM2() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		// n m 입력
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		// 수열 저장 위함
+		res = new int[m];
+		
+		backtracking(0, 1);	// 0번째 단계의 시작 숫자 1
+	}
+	
+	public static void backtracking(int depth, int start) {
+		if(depth == m)	{
+			// m개를 골랐으면 출력
+			for(int i=0; i<m; i++) {
+				System.out.print(res[i]+" ");
+			}
+			System.out.println();
+			return;
+		}
+		
+		for(int i=start; i<=n; i++	) {
+			res[depth] = i;
+			backtracking(depth+1, i+1);
+		}
+	}
+}
---
+++ 2_Java/Study_BaekJoon/src/com/lv20/BackTracking/lv20_3_NandM3.java
@@ -0,0 +1,51 @@
+package com.lv20.BackTracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class lv20_3_NandM3 {
+
+	public static void main(String[] args) throws IOException {
+		nAndM();
+		System.out.println(sb);
+	}
+
+	static int n, m;
+	static int[] res;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void nAndM() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		// n m 입력
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		// 수열 저장 위함
+		res = new int[m];
+		
+		backtracking(0);
+	}
+	
+	public static void backtracking(int depth) {
+		if(depth == m)	{
+			// m개를 골랐으면 출력
+			for(int i=0; i<m; i++) {
+//				System.out.print(res[i]+" ");  시간초과
+				sb.append(res[i]).append(" ");
+			}
+			sb.append("\n");
+//			System.out.println();
+			return;
+		}
+		
+		for(int i=1; i<=n; i++	) {
+			res[depth] = i;
+			backtracking(depth+1);
+		}
+	}
+}
